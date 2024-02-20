e034b61809fb72a0cf00e0f60c2991b2cd0ea5b5
Ahmet <112516305+KimeRti@users.noreply.github.com>
2024-02-20 22:22:43 +0300

    fix

--- MoonBrothers/MoonBrothers/settings/production.py
+++ MoonBrothers/MoonBrothers/settings/production.py
@@ -6,21 +6,16 @@ ALLOWED_HOSTS = ['www.deryamakine.com', 'deryamakine.com']
 
 STATIC_ROOT = 'MoonBrothers/static/'
 
-CSRF_TRUSTED_ORIGINS = ['http://www.deryamakine.com', 'http://deryamakine.com', 'https://www.deryamakine.com', 'https://deryamakine.com']
-CORS_ALLOWED_ORIGINS = ['http://www.deryamakine.com', 'http://deryamakine.com', 'https://www.deryamakine.com', 'https://deryamakine.com']
-CORS_ALLOWED_ORIGIN_REGEXES = ['http://www.deryamakine.com', 'http://deryamakine.com', 'https://www.deryamakine.com', 'https://deryamakine.com']
-CORS_ALLOW_ALL_ORIGINS = ['http://www.deryamakine.com', 'http://deryamakine.com', 'https://www.deryamakine.com', 'https://deryamakine.com']
-
 DATABASES = {
     'default': {
 
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
 
-        'NAME': 'moonbrotherweb',
+        'NAME': 'moonrp',
 
-        'USER': 'moonbrother',
+        'USER': 'detex',
 
-        'PASSWORD': 'moon123',
+        'PASSWORD': 'detex123',
 
         'HOST': 'localhost',
 
