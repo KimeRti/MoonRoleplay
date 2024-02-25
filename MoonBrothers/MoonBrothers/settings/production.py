37375c512aeda6827a69d6bf54896832c5b897e6
Ahmet <112516305+KimeRti@users.noreply.github.com>
2024-02-25 04:42:58 +0300

    Update production.py

--- MoonBrothers/MoonBrothers/settings/production.py
+++ MoonBrothers/MoonBrothers/settings/production.py
@@ -4,6 +4,11 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['5.180.106.89','moonroleplaytr.com','www.moonroleplaytr.com']
 
+CSRF_TRUSTED_ORIGINS = ['http://www.moonroleplaytr.com', 'http://moonroleplaytr.com', 'https://www.moonroleplaytr.com', 'https://moonroleplaytr.com']
+CORS_ALLOWED_ORIGINS = ['http://www.moonroleplaytr.com', 'http://moonroleplaytr.com', 'https://www.moonroleplaytr.com', 'https://moonroleplaytr.com']
+CORS_ALLOWED_ORIGIN_REGEXES = ['http://www.moonroleplaytr.com', 'http://moonroleplaytr.com', 'https://www.moonroleplaytr.com', 'https://moonroleplaytr.com']
+CORS_ALLOW_ALL_ORIGINS = ['http://www.moonroleplaytr.com', 'http://moonroleplaytr.com', 'https://www.moonroleplaytr.com', 'https://moonroleplaytr.com']
+
 STATIC_ROOT = 'MoonBrothers/static/'
 
 DATABASES = {
