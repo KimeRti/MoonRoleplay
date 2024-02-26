14b79068811f1dc30c54ba35a056e2b71cc36f8d
Ahmet <112516305+KimeRti@users.noreply.github.com>
2024-02-27 01:59:41 +0300

    adminpanelfix

--- MoonBrothers/MoonBrothers/settings/base.py
+++ MoonBrothers/MoonBrothers/settings/base.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -110,6 +111,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
 # Default primary key field type
